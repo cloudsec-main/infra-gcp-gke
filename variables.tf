914ba53c574d40a5dd4045ae77ee0062f9767656
netstarz77 <47049785+netstarz77@users.noreply.github.com>
2022-09-01 08:35:44 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,3 +16,17 @@ variable "subnet_cidr" {
 variable "gke_num_nodes" {
   default = 1
 }
+variable "user" {
+    type = string
+}
+variable "email" {
+    type = string
+}
+variable "privatekeypath" {
+    type = string
+    default = "~/.ssh/id_rsa"
+}
+variable "publickeypath" {
+    type = string
+    default = "~/.ssh/id_rsa.pub"
+}
